c25e93fbea5d2fc41ac33cb2f74409d17e1d4cb3
yarolys <lysov.yaroslav.r@mail.ru>
2025-01-14 19:19:55 +0800

    feat(router): add callback query handlers for user management panels
    - Implemented callback query handlers for managing:
      - Schedule (`schedule_management_handler`)
      - Clients (`clients_handler`)
      - Records (`records_handler`)
      - Settings (`settings_handler`)
    - Added `back_to_main_handler` to return users to the main management panel with a personalized welcome message.
    - Enhanced routing functionality with dedicated handlers for improved modularity and user interaction flow.
    - Updated reply markup to dynamically adjust based on the user's selected action.

--- src/handlers/panel_for_user/user_panel.py
+++ src/handlers/panel_for_user/user_panel.py
@@ -1,19 +1,53 @@
 from aiogram import F, Router
 from aiogram.fsm.context import FSMContext
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 from aiogram.filters import Command
 from src.config import logger
 
 from src.utils.filter import AdminRoleFilter
-from src.utils.keyboard.user import master_panel_inline_kb
+from src.utils.keyboard.user import master_panel_inline_kb, schedule_management_kb, clients_kb, records_kb, settings_kb
 
 router = Router()
 
 
-@router.message(Command('user'))
-@router.message(F.text == 'Назад')
-async def admin_panel(message: Message, state: FSMContext):
-    await message.answer('Добро пожаловать в меню пользователя!',
-                        reply_markup=master_panel_inline_kb)
-    await state.clear()
-    await message.delete()
+#Расписание
+@router.callback_query(lambda c: c.data == 'schedule_management')
+async def schedule_management_handler(callback_query: CallbackQuery):
+    await callback_query.message.edit_text(
+        "Управление расписанием:", reply_markup=schedule_management_kb
+    )
+
+
+#Клиенты
+@router.callback_query(lambda c: c.data == 'clients')
+async def clients_handler(callback_query: CallbackQuery):
+    await callback_query.message.edit_text(
+        'Управление клиентами:', reply_markup=clients_kb
+    )
+
+
+#Записи
+@router.callback_query(lambda c: c.data == 'records')
+async def records_handler(callback_query: CallbackQuery):
+    await callback_query.message.edit_text(
+        'Управлением записями:', reply_markup=records_kb
+    )
+
+
+@router.callback_query(lambda c: c.data == 'settings')
+async def settings_handler(callback_query: CallbackQuery):
+    await callback_query.message.edit_text(
+        'Управление настройками:', reply_markup=settings_kb
+    )
+
+
+#Назад
+@router.callback_query(lambda c: c.data == 'back_to_main')
+async def back_to_main_handler(callback_query: CallbackQuery):
+    user_name = callback_query.from_user.full_name 
+    await callback_query.message.edit_text(
+        f'👩🏻 Добро пожаловать, {user_name}. Это Ваш кабинет для управления расписанием и записи клиентов\n'
+        'Ссылка на кабинет для перехода клиентов: \n\n'
+        'Возникли вопросы? Пишите @KandyBobby',
+        reply_markup=master_panel_inline_kb
+    )
