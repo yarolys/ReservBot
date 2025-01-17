3c7d8b67247110083a7d73e4f7342b2bccb739f8
yarolys <lysov.yaroslav.r@mail.ru>
2025-01-17 18:27:25 +0800

    added schedule router

--- src/handlers/__init__.py
+++ src/handlers/__init__.py
@@ -1,3 +1,4 @@
 from .start import router as start_router
 from .panel_for_admin.admin_panel import router as admin_panel_router
 from .panel_for_user.user_panel import router as user_panel_router
+from .panel_for_user.schedule_management import router as schedule_management_router
